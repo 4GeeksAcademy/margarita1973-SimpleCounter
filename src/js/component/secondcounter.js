566333b4481909014fc1ff166921ad21f240e12e
Rosario Alonso <79224753+margarita1973@users.noreply.github.com>
2024-10-07 18:19:33 +0000

    eliminacion de definicion de variables repetidas

--- src/js/component/secondcounter.js
+++ src/js/component/secondcounter.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import Digits from './digits';
 
-let seconds =0
-const SecondCounter = (props) => {
-    let second = Math.floor(seconds) % 10;
-    let tens = Math.floor(seconds / 10) % 10;
-    let hundreds = Math.floor(seconds / 100) % 10;
-    let thousands = Math.floor(seconds / 1000) % 10;
-    let tenthousands = Math.floor(seconds / 10000) % 10;
-    let hundredthousands = Math.floor(seconds / 100000) % 10;
 
+const SecondCounter = (props) => {
+    
     return (<div className="d-flex bg-black d-block m-3 row justify-content-center text-center">
         <div className="digitBox col-2 ">
             <i className="far fa-clock pt-3"></i>
